8bacaa4ab30dac7a85117fd461ceb18b5c658319
Yahya Hussein <yahyahussein91@gmail.com>
2017-02-19 14:30:44 -0800

    set up route for landing page

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 var express = require("express");
 var app = express();
 
+
+//landing page route
+app.get("/", function(req,res){
+	res.send("This will be the landing page");
+});
+
 app.listen(3000, function(){
 	console.log("You are now listening to the smooth sounds of port3000");
 });
