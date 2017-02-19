77f5077c3ebcada678aa6c89fcf091377f47a43a
Yahya Hussein <yahyahussein91@gmail.com>
2017-02-19 14:33:40 -0800

    set up route for campgrounds

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 var express = require("express");
 var app = express();
 
+//set view engine to ejs
 app.set("view engine", "ejs");
+
 //landing page route
 app.get("/", function(req,res){
 	res.send("This will be the landing page");
 });
 
+app.get("/campgrounds", function(req,res) {
+	var
+});
+
+//port for app to be displayed
 app.listen(3000, function(){
 	console.log("You are now listening to the smooth sounds of port3000");
 });
