b3805bb4c9f7ccad8b8ae3715d00d4a6b577061f
Yahya Hussein <yahyahussein91@gmail.com>
2017-02-22 22:02:58 -0800

    reformatted var declarations

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-var express = require("express");
-var app = express();
-var bodyParser = require("body-parser");
-var mongoose = require("mongoose");
+var express 		= require("express"),
+		app 				= express(),
+		bodyParser 	= require("body-parser"),
+	 	mongoose 		= require("mongoose");
 
 //use body parser
 app.use(bodyParser.urlencoded({extended: true}));
