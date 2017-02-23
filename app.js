b5f3c6d0ff268a993f6ca9a8db209c5deb5cd0e1
Yahya Hussein <yahyahussein91@gmail.com>
2017-02-22 22:04:37 -0800

    start of setting up schema

--- app.js
+++ app.js
@@ -9,6 +9,10 @@ app.use(bodyParser.urlencoded({extended: true}));
 //set view engine to ejs
 app.set("view engine", "ejs");
 
+
+
+//Schema setup
+
 var campgrounds = [
 		{name: "Salmon Creek", image: "https://farm8.staticflickr.com/7252/7626464792_3e68c2a6a5.jpg"},
 		{name: "Granite Hill", image: "https://farm9.staticflickr.com/8225/8524305204_43934a319d.jpg"},
