e223a176ee6be494901bca065940ceaf5d981161
Yahya Hussein <yahyahussein91@gmail.com>
2017-03-12 20:19:46 -0700

    added comments to campgrounds

--- seeds.js
+++ seeds.js
@@ -29,11 +29,25 @@ function seedDB(){
 		console.log("removed campgrounds");
 		//add a few campgrounds
 		data.forEach(function(seed){
-			Campground.create(seed, function(err,data){
+			Campground.create(seed, function(err,campground){
 				if(err){
 					console.log(err);
 				}else {
 					console.log("Added campground");
+					//create comment
+					Comment.create(
+						{
+							text: "This place is great but I wish there was internet",
+							author: "Homer"
+						}, function(err,comment){
+							if(err){
+								console.log(err);
+							}else{
+								campground.comments.push(comment);
+								campground.save;
+							}
+								
+						});
 				}
 			});
 		});
