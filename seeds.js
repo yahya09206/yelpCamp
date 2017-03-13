11c4dbfb269b7218c77d530cc9947ae5cc30a71f
Yahya Hussein <yahyahussein91@gmail.com>
2017-03-12 20:12:19 -0700

    added seed data

--- seeds.js
+++ seeds.js
@@ -1,8 +1,27 @@
 var mongoose = require("mongoose");
 var Campground = require("./models/campground");
 
+var data = [
+	{
+		name: "Cloud's rest",
+		image: "https://farm4.staticflickr.com/3872/14435096036_39db8f04bc.jpg",
+		description: "blah blah blah"
+	},
+	{
+		name: "Cloud's rest",
+		image: "https://farm4.staticflickr.com/3872/14435096036_39db8f04bc.jpg",
+		description: "blah blah blah"
+	},
+	{
+		name: "Cloud's rest",
+		image: "https://farm4.staticflickr.com/3872/14435096036_39db8f04bc.jpg",
+		description: "blah blah blah"
+	}
+
+]
 
 function seedDB(){
+	//Remove all campgrounds
 	Campground.remove({}, function(err){
 	if(err){
 		console.log(err);
@@ -10,6 +29,10 @@ function seedDB(){
 		console.log("removed campgrounds");
 	}
 });
+	//add a few campgrounds
+	Campground.create({}, function(){
+
+	});
 }
 
 module.exports = seedDB;
