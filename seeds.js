a7fa44ba298fd85165ebdb3b81f90b4c9337d803
Yahya Hussein <yahyahussein91@gmail.com>
2017-03-12 20:07:42 -0700

    created function for seeding db

--- seeds.js
+++ seeds.js
@@ -1,10 +1,15 @@
 var mongoose = require("mongoose");
 var Campground = require("./models/campground");
 
-Campground.remove({}, function(err){
+
+function seedDB(){
+	Campground.remove({}, function(err){
 	if(err){
 		console.log(err);
 	}else {
 		console.log("removed campgrounds");
 	}
 });
+}
+
+module.exports = seedDB;
