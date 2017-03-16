c7b4f83190d2340c46d3accdb620efda58b80fbc
Yahya Hussein <yahyahussein91@gmail.com>
2017-03-15 18:48:31 -0700

    looping through comments on show page

--- app.js
+++ app.js
@@ -61,6 +61,7 @@ app.get("/campgrounds/:id", function(req,res){
 		if(err){
 			console.log(err)
 		}else{
+			console.log(foundCampground)
 			//render show template with that campground
 			res.render("show", {campground: foundCampground});
 		}
