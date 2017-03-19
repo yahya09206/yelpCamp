7e95c8b7684c294841b5aed8635ca267f84eef56
Yahya Hussein <yahyahussein91@gmail.com>
2017-03-18 20:04:17 -0700

    seeded db with more description text using lorem

--- seeds.js
+++ seeds.js
@@ -6,17 +6,17 @@ var data = [
 	{
 		name: "Cloud's rest",
 		image: "https://farm4.staticflickr.com/3872/14435096036_39db8f04bc.jpg",
-		description: "blah blah blah"
+		description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent fringilla fermentum dictum. Sed tortor sem, sagittis sed sollicitudin et, tempor eu justo. Phasellus faucibus semper erat vitae laoreet. Donec iaculis, risus venenatis imperdiet rutrum, ante orci convallis enim, vel ultricies mi orci et diam. Quisque non ex non leo mollis pulvinar. Nulla facilisi. Donec laoreet orci sed neque tristique aliquam. In porttitor blandit libero non hendrerit. Suspendisse dictum, elit in aliquam gravida, nunc velit ultrices magna, vitae pellentesque elit lorem quis quam. Curabitur faucibus, metus ut dictum viverra, nibh est semper odio, vitae placerat felis sapien vel leo. Ut lacus lacus, euismod vel rutrum sed, gravida in velit. Curabitur tempus cursus nulla at vestibulum. Aenean bibendum pulvinar enim, ut tincidunt ante vulputate nec. Suspendisse potenti."
 	},
 	{
 		name: "Desert Mesa",
 		image: "https://farm7.staticflickr.com/6089/6094103869_d53a990c83.jpg",
-		description: "blah blah blah"
+		description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent fringilla fermentum dictum. Sed tortor sem, sagittis sed sollicitudin et, tempor eu justo. Phasellus faucibus semper erat vitae laoreet. Donec iaculis, risus venenatis imperdiet rutrum, ante orci convallis enim, vel ultricies mi orci et diam. Quisque non ex non leo mollis pulvinar. Nulla facilisi. Donec laoreet orci sed neque tristique aliquam. In porttitor blandit libero non hendrerit. Suspendisse dictum, elit in aliquam gravida, nunc velit ultrices magna, vitae pellentesque elit lorem quis quam. Curabitur faucibus, metus ut dictum viverra, nibh est semper odio, vitae placerat felis sapien vel leo. Ut lacus lacus, euismod vel rutrum sed, gravida in velit. Curabitur tempus cursus nulla at vestibulum. Aenean bibendum pulvinar enim, ut tincidunt ante vulputate nec. Suspendisse potenti."
 	},
 	{
 		name: "Canyon Floor",
 		image: "https://farm3.staticflickr.com/2353/2069978635_2eb8b33cd4.jpg",
-		description: "blah blah blah"
+		description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent fringilla fermentum dictum. Sed tortor sem, sagittis sed sollicitudin et, tempor eu justo. Phasellus faucibus semper erat vitae laoreet. Donec iaculis, risus venenatis imperdiet rutrum, ante orci convallis enim, vel ultricies mi orci et diam. Quisque non ex non leo mollis pulvinar. Nulla facilisi. Donec laoreet orci sed neque tristique aliquam. In porttitor blandit libero non hendrerit. Suspendisse dictum, elit in aliquam gravida, nunc velit ultrices magna, vitae pellentesque elit lorem quis quam. Curabitur faucibus, metus ut dictum viverra, nibh est semper odio, vitae placerat felis sapien vel leo. Ut lacus lacus, euismod vel rutrum sed, gravida in velit. Curabitur tempus cursus nulla at vestibulum. Aenean bibendum pulvinar enim, ut tincidunt ante vulputate nec. Suspendisse potenti."
 	}
 
 ]
