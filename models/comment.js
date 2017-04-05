91714e3d5b468813719a5485be5a0f493b908f9a
Yahya Hussein <yahyahussein91@gmail.com>
2017-04-05 00:10:39 -0700

    starting associations for comments

--- models/comment.js
+++ models/comment.js
@@ -2,7 +2,12 @@ var mongoose = require("mongoose");
 
 var commentSchema = mongoose.Schema({
 	text: String,
-	author: String
+	author: {
+		id: {
+			type: mongoose.Schema.types.ObjectId,
+		}
+		username:
+	}
 });
 
 module.exports = mongoose.model("Comment", commentSchema);
