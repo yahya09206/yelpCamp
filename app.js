aa16d3fa1cbba65de20672d0f3e00ab6e0c37de4
Yahya Hussein <yahyahussein91@gmail.com>
2017-04-04 19:17:39 -0700

    logic that shows if theres a user

--- app.js
+++ app.js
@@ -44,7 +44,7 @@ app.get("/campgrounds", function(req,res) {
 		if(err){
 			console.log(err);
 		}else{
-			res.render("campgrounds/index", {campgrounds: allCampgrounds});
+			res.render("campgrounds/index", {campgrounds: allCampgrounds, currentUser: req.user});
 		}
 	});
 });
