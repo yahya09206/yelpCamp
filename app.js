ed549ea3a5bc93df081d63082cc0e17d2f3f0919
Yahya Hussein <yahyahussein91@gmail.com>
2017-04-04 18:51:15 -0700

    starting on login route

--- app.js
+++ app.js
@@ -140,6 +140,11 @@ app.post("/register", function(req,res){
 	});
 });
 
+//show login form
+app.get("/login", function(req,res){
+	res.render("login")
+})
+
 //port for app to be displayed
 app.listen(3000, function(){
 	console.log("You are now listening to the smooth sounds of port3000");
