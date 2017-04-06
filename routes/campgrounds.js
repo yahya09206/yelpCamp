b26a17c25e5bdfa1f3cb31ee1457830e61e13a3d
Yahya Hussein <yahyahussein91@gmail.com>
2017-04-05 18:33:24 -0700

    added isloggedin function to campgrounds route

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -50,5 +50,13 @@ router.get("/:id", function(req,res){
 	});
 });
 
+//logged in middleware
+function isLoggedIn(req, res, next){
+	if(req.isAuthenticated()){
+		return next();
+	}
+	res.redirect("/login");
+}
+
 
 module.exports = router;
