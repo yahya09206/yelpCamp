7d48293b46bab37816b47cd028993f56baece076
Yahya Hussein <yahyahussein91@gmail.com>
2017-04-06 19:46:40 -0700

    added campground authorization to middleware file

--- middleware/index.js
+++ middleware/index.js
@@ -1,3 +1,26 @@
 //All the middleware will be added here
+var middlewareObj = {};
 
-module.exports = middleObj
+//Campground Authorization
+middleware.checkCampgroundOwnerShip = function(req,res,next){
+	if(req.isAuthenticated()){
+			Campground.findById(req.params.id, function(err, foundCampground){
+				if(err){
+					res.redirect("back")
+				}else{
+				if(foundCampground.author.id.equals(req.user._id)){
+					next();
+				}else{
+					res.redirect("back");
+				}
+				
+			}
+		});
+	}else{
+		res.redirect("back");
+	}
+}
+
+
+
+module.exports = middlewareObj;
